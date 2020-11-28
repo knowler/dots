8e9e381fcf1a427984d73ab18d65f4525569098b
Nathan Knowler <nathan@knowler.me>
2020-11-28 02:38:49 -0600

    Scaffold CLI API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "dots")]
+enum Dots {
+    /// Install new system packages
+    Install,
+
+    /// Upgrade all or specified system packages
+    Upgrade {
+        packages: Vec<String>,
+    },
+
+    /// Run post-install setup for some packages
+    Setup {
+        packages: Vec<String>,
+    },
+
+    /// Dump installed packages to list
+    Dump,
+
+    /// Apply a pre-made patch
+    Patch {
+        patch_files: Vec<String>,
+    },
+    
+    /// Open a dotfile in an editor
+    Open {
+        dotfile: String,
+    },
+
+    /// Open the repo in the browser
+    Web,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Dots::from_args();
+
+    println!("{:?}", matches);
 }
